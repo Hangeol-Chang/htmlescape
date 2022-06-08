808fb3ef31ae0330bf1175e616003765e7eaa3ee
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-06-08 23:21:38 +0900

    react studing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "한걸"
+  const obj = {
+    name : '링크',
+    url : 'https://www.naver.com'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+        color: "#f0f",
+        backgroundColor: "green",
+      }}>
+        Hello, {name}
+      </h1>
+      <a href={obj.url}>{obj.name}</a>
     </div>
   );
 }
