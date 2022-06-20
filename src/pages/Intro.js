d066f30c9309e1a253dae9f4e7eb24197e7dd5ab
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-06-20 19:23:56 +0900

    Update Intro.js

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import { Link } from 'react-router-dom';
+import { blue } from '@mui/material/colors';
 
 export default function Intro() {
 
@@ -13,7 +14,20 @@ export default function Intro() {
             justifyContent : 'center'
         }}>
             <h1>Hello! Welcome to the Html Escape</h1>
-            <Link to="/stage1">Start Escape</Link>
+            <Link to="/stage1"
+                style={
+                    {
+                        color : 'white',
+                        backgroundColor : '#754685',
+                        padding : '10px',
+                        textDecoration : 'none',
+                        borderRadius : '5px',
+                        margin : '10px',
+                    }
+                }
+                >
+                Start Escape
+            </Link>
             <hr></hr>
         </Box>
     );
