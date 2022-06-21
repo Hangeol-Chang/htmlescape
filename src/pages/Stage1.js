0ace97cfd4f4f7c5134e550dad0a4403b1b9aafd
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-06-21 11:49:42 +0900

    update

--- src/pages/Stage1.js
+++ src/pages/Stage1.js
@@ -1,85 +1,70 @@
 import {Box, Button, Typography} from '@mui/material/'
 import { render } from '@testing-library/react';
 import { useState, setState, Component } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 let topbt = [1, 2, 3, 4];
 let botbt = [5, 6, 7, 8];
 
-class Stage1 extends Component {
-    state = {
-        key : '',
-    };
+export default function Stage1() {
+    let [key, setkey] = useState('');
+    const navigate = useNavigate();
 
-    constructor() {
-        super();
-        console.log("stage1 시작");
-        this.setval = this.setval.bind(this);
-    }
-    
-    setval = (val, e) => {
-        console.log(this.state.key + " 누른 값 " + val);
-    
-        // setkey가 비동기로 작동해서, 이를 동기로 바꾸기 위해 콜백함수를 이용.
-        // setkey(key.concat(val));
+    const Setval = (val, e) => {
+        console.log(key + " 누른 값 " + val);
         
-        this.setState({ key : this.state.key.concat(val) }, () => {
-            let keyc = this.state.key;
-            console.log("현재 키 값 : " + keyc + " 키 길이 : " + keyc.length)
-    
-            if(keyc.length == 8) {
-                if(keyc === '18657234'){
-                    console.log("pass")
-                }            
-                else{
-                    console.log('fail')
-                    this.state.key = '';
-                }
+        if(key.length == 7) {
+            if(key === "1685723" && val == 4){
+                console.log("pass");
+                
+                navigate('/stage2');
             }
-        });
+            else {
+                console.log("fail");
+                setkey('');
+            }
+        }
+        else setkey(key.concat(val));
     }
 
-    render() {
+    return (
+        <Box >
+            {topbt.map( n => (
+                <Button 
+                    key={n}
+                    variant="outlined"
+                    sx={{ m:3 }}
+                    onClick={e => Setval(n, e)}
+                >
+                    {n}
+                </Button>
+            ))}
+            <h2>Hello! umm... it seems there are some buttons you can click.</h2>
+            <Typography>
+                find the correct order to escape this page.<br></br>
+                to find correct order, you may need to search everywhere you can approach.<br></br>
+                such as... 
+            </Typography>
 
-        return (
-            <Box >
-                {topbt.map( n => (
-                    <Button 
-                        key={n}
-                        variant="outlined"
-                        sx={{ m:3 }}
-                        onClick={e => this.setval(n, e)}
-                    >
-                        {n}
-                    </Button>
-                ))}
-                <h2>Hello! umm... it seems there are some buttons you can click.</h2>
-                <Typography>
-                    find the correct order to escape this page.<br></br>
-                    to find correct order, you may need to search everywhere you can approach.<br></br>
-                    such as... 
-                </Typography>
-    
-                <input type="hidden" placeholder='btn order is 1 8 6 5 7 2 3 4' />
-                
-                <Typography sx={{ fontWeight: 'bold'}}>
-                    developer tools?
-                </Typography>
-                <Typography sx={{ fontSize : '6px' }}>
-                    oops! why line break is occur between ... and developer ?
-                </Typography>
-                
-                {botbt.map( n => (
-                    <Button
-                        key={n}
-                        variant="outlined"
-                        sx={{ m:3 }}
-                        onClick={e => this.setval(n, e)}
-                    >
-                        {n}
-                    </Button>
-                ))}
-            </Box>
-        )
-    }
+            <input type="hidden" placeholder='btn order is 1 6 8 5 7 2 3 4' />
+            
+            <Typography sx={{ fontWeight: 'bold'}}>
+                developer tools?
+            </Typography>
+            <Typography sx={{ fontSize : '6px' }}>
+                oops! why line break is occur between ... and developer ?
+            </Typography>
+            
+            {botbt.map( n => (
+                <Button
+                    key={n}
+                    variant="outlined"
+                    sx={{ m:3 }}
+                    onClick={e => Setval(n, e)}
+                >
+                    {n}
+                </Button>
+            ))}
+        </Box>
+    )
 }
-export default Stage1
--- src/pages/common/Footer.js
+++ src/pages/common/Footer.js
@@ -1,8 +1,21 @@
-import Grid from '@mui/material/Grid';
-import {Button, Typography} from '@mui/material/';
+import { Grid, Icon, SvgIcon } from '@mui/material';
+import {Button, Typography} from '@mui/material';
+import { GitHub, YouTube, Article } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
+    const devinfo = [
+        {
+            title : 'email',
+            value : 'hgchang1@naver.com'
+        },
+        {
+            title : 'instagram',
+            value : '@hihangeol'
+        },
+    ];
+    const copyright = 'ⓒ 2022 hihangeol'
+    // 사이트 업데이트 내역 알려주는거 있으면 괜찮을듯.
     const links = [
         {
             name: 'About',
@@ -19,7 +32,16 @@ export default function Footer() {
     ];
 
     return (
-        <footer style={{background: "#798BBC"}}>
+        <footer style={{
+                background: "#798BBC",
+                color : 'white',
+                padding : '10px',
+            }}>
+            <Grid container spacing={0} justify="space-evenly">
+                <GitHub></GitHub>
+                <YouTube></YouTube>
+                <Article></Article>
+            </Grid>
             <Grid container spacing={0} justify="space-evenly">
                 {links.map(link => (
                     <Grid item xs key={link.name} sx={{ my : 2}}>
@@ -31,6 +53,10 @@ export default function Footer() {
                     </Grid>
                 ))}
             </Grid>
+
+            <div>
+                {copyright}
+            </div>
         </footer>
     )
 }
