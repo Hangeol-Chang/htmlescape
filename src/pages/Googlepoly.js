35d52a2ace449c56df46f5009bd5b6bf1a55b673
장한걸 ( HUD-SW셀 ) <57909592+Hangeol-Chang@users.noreply.github.com>
2022-12-28 11:30:49 +0900

    Update Googlepoly.js

--- src/pages/Googlepoly.js
+++ src/pages/Googlepoly.js
@@ -1,27 +1,22 @@
-import { Box, Button } from "@mui/material";
-import React from "react";
+import { Box, Button, Input, Slider } from "@mui/material";
+import { useState } from "react";
 import { Circle, GoogleMap, LoadScript, Polyline, useJsApiLoader } from '@react-google-maps/api'
 
 export default function Googlepoly() {
-    let [coordi, setCoordi] = React.useState("[[37.772, -122.214],[21.291, -157.821],[18.142, 178.431],[27.467, 153.027]]");
-    let [path, setPath] = React.useState([
+    let [coordi, setCoordi] = useState("[[37.772, -122.214],[21.291, -157.821],[18.142, 178.431],[27.467, 153.027]]");
+    let [path, setPath] = useState([
         {lat: 37.772, lng: -122.214},
         {lat: 21.291, lng: -157.821},
         {lat: -18.142, lng: 178.431},
         {lat: -27.467, lng: 153.027}
     ]);
+    let [zoom, setZoom] = useState(10);
     
-    let [center, setSenter] = React.useState({
-        lat: 37.497952,
-        lng: 127.027619
-    });
+    let [circleRadius, setCirclRadius] = useState(1);
+    let [circleColor, serCircleColor] = useState('#FF0000');
+    let [lineColor, setLineColor] = useState('#00FF00');
 
-    const containerStyle = {
-        width: window.innerWidth * 0.8,
-        height: window.innerHeight * 0.6
-    };
-      
-    const options = {
+    let [lineOptions, setLineOptions] = useState({
         strokeColor: '#0000FF',
         strokeOpacity: 0.8,
         strokeWeight: 2,
@@ -31,9 +26,9 @@ export default function Googlepoly() {
         visible: true,
         radius: 30000,
         zIndex: 1
-    };
+    })
 
-    const coptions = {
+    let [circleOptions, setcircleOptions] = useState({
         strokeOpacity : 0,
         fillColor: '#FF0000',
         fillOpacity: 0.35,
@@ -43,7 +38,17 @@ export default function Googlepoly() {
         visible: true,
         radius: 1,
         zIndex: 1
-    }
+    })
+
+    let [center, setSenter] = React.useState({
+        lat: 37.497952,
+        lng: 127.027619
+    });
+
+    const containerStyle = {
+        width: window.innerWidth * 0.8,
+        height: window.innerHeight * 0.6
+    };
 
     function makeline() {
         let aa = coordi
@@ -99,26 +104,25 @@ export default function Googlepoly() {
                 이거슨 버튼
             </Button>
             
+            원 크기 조정
+            <Slider></Slider>
+            선 색상 입력
+            <Input></Input>
+            원 색상 입력
+            <Input></Input>
             <LoadScript
                 googleMapsApiKey="AIzaSyBkZS2y5XLGTz09p372w0MV4bQgeukEiiQ"
             >
                 <GoogleMap
                     mapContainerStyle={containerStyle}
                     center={center}
-                    zoom={10}
-                >
-                { /* Child components, such as markers, info windows, etc. */ }
-                
-                    <Polyline
-                        path={path}
-                        options={options}
-                    >
-
-                    </Polyline>
+                    zoom={zoom}
+                >                
+                    <Polyline path={path} options={lineOptions} ></Polyline>
 
                     {
                         path.map(c => (
-                            <Circle center={c} key={c[0]} options={coptions}></Circle>
+                            <Circle center={c} key={c[0]} options={circleOptions}></Circle>
                         ))
                     }
 
